9244a6fb988f7aee087250ec1f40000022296084
WalterHorst <whorst42@gmail.com>
2023-07-22 21:22:56 -0300

    Form casi listo

--- client/src/views/Form page/Form.jsx
+++ client/src/views/Form page/Form.jsx
@@ -1,8 +1,69 @@
+import { useState } from "react";
+import "./Form.css";
+
 const Form = () => {
+  const [form, setForm] = useState({
+    nombre: "",
+    resumen: "",
+    healtScore: "",
+    pasos: "",
+    imagen: "",
+  });
+
+  const changeHandler = (event) => {
+    const property = event.target.name;
+    const value = event.target.value;
+    setForm({ ...form, [property]: value });
+  };
+
   return (
-    <div>
-      <h1>Esta es la vista de Form</h1>
-    </div>
+    <form>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Nombre"
+          value={form.nombre}
+          name="nombre"
+          onChange={changeHandler}
+        ></input>
+      </div>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Resumen del plato"
+          value={form.resumen}
+          name="resumen"
+          onChange={changeHandler}
+        ></input>
+      </div>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="HealtScore"
+          value={form.healtScore}
+          name="healtScore"
+          onChange={changeHandler}
+        ></input>
+      </div>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Pasos"
+          value={form.pasos}
+          name="pasos"
+          onChange={changeHandler}
+        ></input>
+      </div>
+      <div className="container">
+        <input
+          type="file"
+          value={form.imagen}
+          name="imagen"
+          onChange={changeHandler}
+        ></input>
+      </div>
+      <button type="submit">Crear receta</button>
+    </form>
   );
 };
 export default Form;
